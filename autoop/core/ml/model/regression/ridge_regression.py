ee54a410252ede2c0018e1018c6164c88bf17863
Iveto3 <i.i.ivanova@student.rug.nl>
2024-10-20 18:01:07 +0200

    typing for regression models

--- autoop/core/ml/model/regression/ridge_regression.py
+++ autoop/core/ml/model/regression/ridge_regression.py
@@ -1,10 +1,11 @@
 from sklearn.linear_model import Ridge
-#RIDGE MODEL NAME FILE CHANGE!!!!!!
+import numpy as np
+
 
 class RidgeRegression(Ridge):
 
-    def fit(self, train_X, train_y):
+    def fit(self, train_X: np.ndarray, train_y: np.ndarray) -> None:
         super().fit(self, train_X, train_y)
 
-    def predict(self, test_X):
+    def predict(self, test_X: np.ndarray) -> np.ndarray:
         return super().predict(self, test_X)
