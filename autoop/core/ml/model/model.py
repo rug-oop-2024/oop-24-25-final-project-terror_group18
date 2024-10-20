cd8aca4506c5b7d65c55c9fd371588e9593d9d88
Iveto3 <i.i.ivanova@student.rug.nl>
2024-10-20 19:06:29 +0200

    Model base class created

--- autoop/core/ml/model/model.py
+++ autoop/core/ml/model/model.py
@@ -4,8 +4,19 @@ from autoop.core.ml.artifact import Artifact
 import numpy as np
 from copy import deepcopy
 from typing import Literal
+from pydantic import BaseModel
 
-class Model:
+
+class Model(BaseModel):
     type: str
-    pass # your code (attribute and methods) here
-    
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def fit(self, train_X: np.ndarray, train_y: np.ndarray) -> None:
+        pass  # your code (attribute and methods) here
+
+    @abstractmethod
+    def predict(self, test_X: np.ndarray) -> np.ndarray:
+        pass
