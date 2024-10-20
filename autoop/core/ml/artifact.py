11ad71f9b1e6db6997a22612f9e7cc80f427208a
KTtheBugmaker2000 <katya.toncheva@gmail.com>
2024-10-20 19:48:05 +0200

    artifact impl start

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -3,4 +3,19 @@ import base64
 
 
 class Artifact(BaseModel):
-    pass
+    asset: dict
+
+    def __init__(self, name: str,
+            version: float,
+            asset_path: str,
+            tags: list,
+            metadata: dict,
+            data,
+            type: str):
+
+        super().__init__()
+        self.asset["name"] = name
+        self.asset["version"] = version
+
+
+
