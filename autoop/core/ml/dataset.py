f4c7429c9e4b487b906baa1ed0b6b51ace0f1195
Iveto3 <i.i.ivanova@student.rug.nl>
2024-10-20 19:21:56 +0200

    feature class created + flake8 edits

--- autoop/core/ml/dataset.py
+++ autoop/core/ml/dataset.py
@@ -9,7 +9,8 @@ class Dataset(Artifact):
         super().__init__(type="dataset", *args, **kwargs)
 
     @staticmethod
-    def from_dataframe(data: pd.DataFrame, name: str, asset_path: str, version: str="1.0.0"):
+    def from_dataframe(data: pd.DataFrame, name: str, asset_path: str,
+                       version: str = "1.0.0"):
         return Dataset(
             name=name,
             asset_path=asset_path,
@@ -21,8 +22,7 @@ class Dataset(Artifact):
         bytes = super().read()
         csv = bytes.decode()
         return pd.read_csv(io.StringIO(csv))
-    
+
     def save(self, data: pd.DataFrame) -> bytes:
         bytes = data.to_csv(index=False).encode()
         return super().save(bytes)
-    
--- autoop/core/ml/feature.py
+++ autoop/core/ml/feature.py
@@ -2,10 +2,18 @@
 from pydantic import BaseModel, Field
 from typing import Literal
 import numpy as np
-
 from autoop.core.ml.dataset import Dataset
 
+
 class Feature(BaseModel):
+    name: str = Field(default_factory=str)
+    type: Literal["categorical", "numerical"] = Field(default="categorical")
+    # do we need default in Field and in __init__???
+
+    def __init__(self, name: str = None,
+                 type: Literal["categorical", "numerical"] = "categorical"):
+        super().__init__(name=name, type=type)
+
     # attributes here
 
     def __str__(self):
