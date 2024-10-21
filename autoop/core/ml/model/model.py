4a3e4df9132f4a19c6f6b7e951562c65c46157c2
KTtheBugmaker2000 <katya.toncheva@gmail.com>
2024-10-21 15:00:51 +0200

    model class attribute additions

--- autoop/core/ml/model/model.py
+++ autoop/core/ml/model/model.py
@@ -1,23 +1,28 @@
 
 from abc import abstractmethod, ABC
+
+from pydantic import Field
+
 from autoop.core.ml.artifact import Artifact
 import numpy as np
 from copy import deepcopy
 from typing import Literal
 
 
-class Model(ABC):
-    type: str
+class Model(ABC):   # Artifact?
+    name: str = "model"
+    type: Literal["classification", "regression"] = (
+        Field(default_factory=Literal["classification", "regression"]))
 
-    def __init__(self):
-        pass
+    '''def __init__(self):
+        pass'''
 
     def __str__(self):
         return f"Model(type={self.type})"
 
     @abstractmethod
     def fit(self, train_X: np.ndarray, train_y: np.ndarray) -> None:
-        pass  # your code (attribute and methods) here
+        pass
 
     @abstractmethod
     def predict(self, test_X: np.ndarray) -> np.ndarray:
