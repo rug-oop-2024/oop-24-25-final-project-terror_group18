7438d4be9fc399d651e03cb8b936fea32f997046
Iveto3 <i.i.ivanova@student.rug.nl>
2024-10-21 12:14:35 +0200

    Model edits

--- autoop/core/ml/model/model.py
+++ autoop/core/ml/model/model.py
@@ -4,14 +4,16 @@ from autoop.core.ml.artifact import Artifact
 import numpy as np
 from copy import deepcopy
 from typing import Literal
-from pydantic import BaseModel
 
 
-class Model(BaseModel):
+class Model():
     type: str
 
     def __init__(self):
         pass
+
+    def __str__(self):
+        return f"Model(type={self.type})"
 
     @abstractmethod
     def fit(self, train_X: np.ndarray, train_y: np.ndarray) -> None:
@@ -20,3 +22,6 @@ class Model(BaseModel):
     @abstractmethod
     def predict(self, test_X: np.ndarray) -> np.ndarray:
         pass
+
+    def to_artifact(self, name: str) -> Artifact:
+        return Artifact(name=name, model=deepcopy(self))
