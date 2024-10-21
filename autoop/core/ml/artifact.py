0c3ef5437c51d88d5124fc49b298304d132ae6fa
KTtheBugmaker2000 <katya.toncheva@gmail.com>
2024-10-21 12:17:13 +0200

    artifact changes

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -13,12 +13,12 @@ class Artifact(BaseModel):
                  metadata: dict = {},
                  data: str = "N/A",
                  type: str = "N/A", **kwargs):
-
         super().__init__()
         self.id = base64.b64encode(
             asset_path.encode('utf-8')
-            ).decode('utf-8')
+        ).decode('utf-8')
 
+        self.asset["artifact_id"] = self.id
         self.asset["name"] = name
         self.asset["version"] = version
         self.asset["asset_path"] = asset_path
@@ -27,15 +27,21 @@ class Artifact(BaseModel):
         self.asset["data"] = data
         self.asset["type"] = type
 
-        for key, value
-
-
+        for key, value in kwargs:
+            setattr(self, key, value)
 
     def read(self) -> bytes:
         """
         Read artifact data
         """
         return base64.b64decode(self.asset["artifact_id"])
-    
+
     def save(self, data: bytes):
+        """
+        Args:
+            data:
+
+        Returns:
+
+        """
         self.asset["data"] = base64.b64encode(data)
