ae86480b5af3d1fc6816f1660500792d2768815c
KTtheBugmaker2000 <katya.toncheva@gmail.com>
2024-11-01 18:12:26 +0100

    uploading files handling

--- app/pages/1_📊_Datasets.py
+++ app/pages/1_📊_Datasets.py
@@ -10,10 +10,13 @@ automl = AutoMLSystem.get_instance()
 datasets = automl.registry.list(type="dataset")
 
 # option = st.selectbox(glob("**/*.csv", recursive=True))
-option = st.selectbox("Dataset", glob("**/*.csv", recursive=True))
-if option is None:
+option = st.file_uploader("Choose a file")
+#option = st.selectbox("Load Previous Dataset", glob("**/*.csv", recursive=True))
+if option is not None:
+    df = pd.read_csv(option)
+    st.write(df.head())
+    options = st.multiselect(df.columns)
+else:
     st.write('Please select something')
 
-df = pd.read_csv(option)
-st.write(df.head())
-options = st.multiselect(df.columns)
+
