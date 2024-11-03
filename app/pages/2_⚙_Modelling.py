340a6b7231a0a816c7fa5745712af0edddb6899e
Iveto3 <i.i.ivanova@student.rug.nl>
2024-11-03 14:22:39 +0100

    modelling edits

--- app/pages/2_⚙_Modelling.py
+++ app/pages/2_⚙_Modelling.py
@@ -16,13 +16,26 @@ write_helper_text("In this section, you can design a machine learning pipeline t
 automl = AutoMLSystem.get_instance()
 
 datasets = automl.registry.list(type="dataset")
+# print(datasets)
 
+data = pd.read_csv(r"C:\Users\Iva\Downloads\wine+quality\winequality-red.csv")
+st.write(data.columns)
 # features_selection = st.column_config.SelectboxColumn(label=None, width=None, help=None, disabled=None, required=None, default=None, options=None)
 
- 
+# data = pd.read_csv(datasets)
 
-# if features_selection is not None:
-#     dataset = automl.registry.get(features_selection)
-#     st.write(dataset.read())
+
+for column in data.columns:
+    st.selectbox(
+        f"Select an option for {column}", 
+        (column)
+    )
+
+# # Display the selected column's data
+# st.write(f"You selected {selected_column}")
+# st.write(datasets[selected_column])
+# # if features_selection is not None:
+# #     dataset = automl.registry.get(features_selection)
+# #     st.write(dataset.read())
 
 
