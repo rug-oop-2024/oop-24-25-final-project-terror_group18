bfd954076b830ba4156981f9ca4d68dec9aea66b
Iveto3 <i.i.ivanova@student.rug.nl>
2024-11-10 01:37:19 +0100

    test_pipeline small bug fix

--- autoop/tests/test_pipeline.py
+++ autoop/tests/test_pipeline.py
@@ -50,7 +50,7 @@ class TestPipeline(unittest.TestCase):
         self.pipeline._preprocess_features()
         self.pipeline._split_data()
         self.pipeline._train()
-        self.assertIsNotNone(self.pipeline._model.parameters)
+        self.assertIsNotNone(self.pipeline._model._parameters)
 
     def test_evaluate(self):
         self.pipeline._preprocess_features()
