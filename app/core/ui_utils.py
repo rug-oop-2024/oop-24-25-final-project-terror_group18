93852f7e2464a43883dae8a9d572e225bb46192a
Iveto3 <i.i.ivanova@student.rug.nl>
2024-11-10 02:18:48 +0100

    system + ui_utils flake8

--- app/core/ui_utils.py
+++ app/core/ui_utils.py
@@ -1,16 +1,22 @@
 from copy import deepcopy
-
 import pandas as pd
 import streamlit as st
-
 from app.core.system import AutoMLSystem
 from autoop.core.ml.dataset import Dataset
 
 automl = AutoMLSystem.get_instance()
 datasets = automl.registry.list(type="dataset")
 
+
 class DataHandler:
-    def __init__(self, datasets=datasets):
+    """
+    A class to handle the data upload and display."""
+    def __init__(self, datasets=datasets) -> None:
+        #datasets type??
+        """
+        A constructor for the DataHandler class.
+        ..................................?????????
+        """
         self.df = None
         self._dataset_name_to_id = {dt.name: dt.id for dt in datasets}
         self._options = ["UPLOAD"] + list(self._dataset_name_to_id.keys())
@@ -19,7 +25,11 @@ class DataHandler:
         self._file = None
         self._dataset = None
 
-    def _choose_file(self):
+    def _choose_file(self) -> None:
+        """
+        A method to choose a file to upload.
+        :return: None
+        """
         self._option = st.selectbox("Choose Dataset", self._options)
         if self._option is None:
             st.write('Please select something.')
@@ -35,25 +45,40 @@ class DataHandler:
                 self._file_path = self._file.asset_path
                 self.df = self._file.read()
 
-    def _handle(self):
+    def _handle(self) -> None:
+        """
+        A method to handle the data upload and display.
+        :return: None
+        """
         self.display()
         self._dataset = Dataset.from_dataframe(
             data=self.df, name=self._file_path.removesuffix('.csv'),
             asset_path=self._file_path)
         st.session_state['dataframe'] = self.df
-        #st.session_state['df_dataset'] = self._dataset
-        #st.session_state['dataset_id'] = self._dataset_id
 
     @property
-    def dataset(self):
+    def dataset(self) -> Dataset:
+        """
+        A getter method for the dataset attribute.
+        :return: The dataset.
+        """
         return deepcopy(self._dataset)
 
-    def display(self):
+    def display(self) -> None:
+        """
+        A method to display the data.
+        :return: None
+        """
         st.write(self.df.head())
         st.write(f"You chose {self._file_path.removesuffix('.csv')}")
 
     @staticmethod
     def save_in_registry(dataset: Dataset) -> bool:
+        """
+        A method to save the dataset in the registry.
+        :param dataset: The dataset to save.
+        :return: True if the dataset was saved, False otherwise.
+        """
         try:
             if dataset.asset_path not in [x.asset_path for x in datasets]:
                 automl.registry.register(dataset)
@@ -62,7 +87,11 @@ class DataHandler:
         except AttributeError:
             raise AttributeError(f"dataset is {dataset}")
 
-    def run(self):
+    def run(self) -> None:
+        """
+        A method to run the data handler.
+        :return: None
+        """
         self._choose_file()
         if self._file is not None:
             self._handle()
@@ -71,5 +100,7 @@ class DataHandler:
                     if self.save_in_registry(self._dataset):
                         st.write("Dataset saved.")
                     else:
-                        st.write(f"Dataset with path {self._dataset.asset_path} already exists. "
-                                 f"Please select another dataset.")
+                        st.write(
+                            f"Dataset with path {self._dataset.asset_path}"
+                            " already exists. "
+                            "Please select another dataset.")
