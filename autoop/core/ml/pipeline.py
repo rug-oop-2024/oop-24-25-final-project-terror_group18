c8b1c1b78e52386997e3ee642ca24e754c5fa451
Iveto3 <i.i.ivanova@student.rug.nl>
2024-11-10 03:35:41 +0100

    ml_type+pipeline flake8

--- autoop/core/ml/pipeline.py
+++ autoop/core/ml/pipeline.py
@@ -1,6 +1,5 @@
 from typing import List
 import pickle
-
 from autoop.core.ml.artifact import Artifact
 from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.model.base_model import Model
@@ -8,19 +7,30 @@ from autoop.core.ml.feature import Feature
 from autoop.core.ml.metric import Metric
 from autoop.functional.preprocessing import preprocess_features
 import numpy as np
-import streamlit as st
 
 
 class Pipeline():
-    
-    def __init__(self, 
+    """
+    The Pipeline class.
+    """
+    def __init__(self,
                  metrics: List[Metric],
-                 dataset: Dataset, 
+                 dataset: Dataset,
                  model: Model,
                  input_features: List[Feature],
                  target_feature: Feature,
-                 split=0.8,
-                 ):
+                 split: float = 0.8,
+                 ) -> None:
+        """
+        The Pipeline class constructor.
+        :param metrics: List[Metric]
+        :param dataset: Dataset
+        :param model: Model
+        :param input_features: List[Feature]
+        :param target_feature: Feature
+        :param split: float
+        :return: None
+        """
         self._dataset = dataset
         self._model = model
         self._input_features = input_features
@@ -29,12 +39,19 @@ class Pipeline():
         self._artifacts = {}
         self._split = split
         if self._model is not None:
-            if target_feature.type == "categorical" and model._type != "classification":
-                raise ValueError("Model type must be classification for categorical target feature")
-            if target_feature.type == "continuous" and model._type != "regression":
-                raise ValueError("Model type must be regression for continuous target feature")
+            if (target_feature.type == "categorical"
+               and model._type != "classification"):
+                raise ValueError("Model type must be classification "
+                                 "for categorical target feature")
+            if (target_feature.type == "continuous"
+               and model._type != "regression"):
+                raise ValueError("Model type must be regression "
+                                 "for continuous target feature")
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """The string representation of the pipeline.
+        :return: str
+        """
         return f"""
 Pipeline(
     model={self._model._type},
@@ -47,11 +64,17 @@ Pipeline(
 
     @property
     def model(self) -> Model:
+        """
+        The getter method for the model object.
+        :return: Model
+        """
         return self._model
 
     @property
     def artifacts(self) -> List[Artifact]:
-        """Used to get the artifacts generated during the pipeline execution to be saved
+        """Used to get the artifacts generated during the pipeline execution
+        to be saved.
+        :return: List[Artifact]
         """
         artifacts = []
         for name, artifact in self._artifacts.items():
@@ -69,14 +92,26 @@ Pipeline(
             "target_feature": self._target_feature,
             "split": self._split,
         }
-        artifacts.append(Artifact(name="pipeline_config", data=pickle.dumps(pipeline_data)))
-        artifacts.append(self._model.to_artifact(name=f"pipeline_model_{self._model.type}"))
+        artifacts.append(Artifact(name="pipeline_config",
+                                  data=pickle.dumps(pipeline_data)))
+        artifacts.append(self._model.to_artifact(
+            name=f"pipeline_model_{self._model.type}"))
         return artifacts
-    
-    def _register_artifact(self, name: str, artifact):
+
+    def _register_artifact(self, name: str, artifact: Artifact) -> None:
+        """
+        The method that registers an artifact in the pipeline.
+        :param name: str
+        :param artifact: Artifact
+        :return: None
+        """
         self._artifacts[name] = artifact
 
-    def _preprocess_features(self):
+    def _preprocess_features(self) -> None:
+        """
+        The method that preprocesses the features.
+        :return: None
+        """
         (target_feature_name, target_data, artifact) = preprocess_features(
             [self._target_feature], self._dataset)[0]
         self._register_artifact(target_feature_name, artifact)
@@ -90,23 +125,44 @@ Pipeline(
         self._input_vectors = [data for (
             feature_name, data, artifact) in input_results]
 
-    def _split_data(self):
+    def _split_data(self) -> None:
+        """
+        The method that splits the data into training and testing sets.
+        :return: None
+        """
         # Split the data into training and testing sets
         split = self._split
-        self._train_X = [vector[:int(split * len(vector))] for vector in self._input_vectors]
-        self._test_X = [vector[int(split * len(vector)):] for vector in self._input_vectors]
-        self._train_y = self._output_vector[:int(split * len(self._output_vector))]
-        self._test_y = self._output_vector[int(split * len(self._output_vector)):]
+        self._train_X = [vector[:int(split * len(vector))]
+                         for vector in self._input_vectors]
+        self._test_X = [vector[int(split * len(vector)):]
+                        for vector in self._input_vectors]
+        self._train_y = self._output_vector[
+            :int(split * len(self._output_vector))]
+        self._test_y = self._output_vector[
+            int(split * len(self._output_vector)):]
 
     def _compact_vectors(self, vectors: List[np.array]) -> np.array:
+        """
+        The method that concatenates the input vectors into a single vector.
+        :param vectors: List[np.array]
+        :return: np.array
+        """
         return np.concatenate(vectors, axis=1)
 
-    def _train(self):
+    def _train(self) -> None:
+        """
+        The method that fits the training data to the model.
+        :return: None
+        """
         X = self._compact_vectors(self._train_X)
         Y = self._train_y
         self._model.fit(X, Y)
 
-    def _evaluate(self):
+    def _evaluate(self) -> None:
+        """
+        The method that evaluates the model on the test data.
+        :return: None
+        """
         X = self._compact_vectors(self._test_X)
         Y = self._test_y
         self._metrics_results = []
@@ -116,18 +172,18 @@ Pipeline(
             self._metrics_results.append((metric, result))
         self._predictions = predictions
 
-    def execute(self):
+    def execute(self) -> None:
+        """
+        The method that executes the pipeline.
+        :return: None
+        """
         self._preprocess_features()
         self._split_data()
         self._train()
         X = self._compact_vectors(self._test_X)
         self._predictions = self._model.predict(X)
-        return self._predictions
-        # self._evaluate()
-        # return {
-        #     "metrics": self._metrics_results,
-        #     "predictions": self._predictions,
-        # }
-        
-
-    
+        self._evaluate()
+        return {
+            "metrics": self._metrics_results,
+            "predictions": self._predictions,
+        }
